df82d3585a0651ff50f5b48579ddfeb533695d96
theo-pombe <theo.pombe@outlook.com>
2025-09-28 19:08:56 +0300

    feat(auth): add request body validation for auth routes

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
 import tryCatch from "../utils/tryCatch.js";
 import AuthController from "../controllers/authController.js";
+import validate from "../middlewares/validate.js";
+import {
+  loginBodySchema,
+  registerBodySchema,
+} from "../schema/authValidation.js";
 
 const authRouter = Router();
 
 authRouter
-  .post("/register", tryCatch(new AuthController().register, "Register"))
-  .post("/login", tryCatch(new AuthController().login, "login"));
+  .post(
+    "/register",
+    validate({ body: registerBodySchema }),
+    tryCatch(new AuthController().register, "Register")
+  )
+  .post(
+    "/login",
+    validate({ body: loginBodySchema }),
+    tryCatch(new AuthController().login, "login")
+  );
 
 export default authRouter;
