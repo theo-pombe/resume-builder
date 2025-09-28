7dd9a12d5c9825b4dff81801b90a9fe59c0aa152
theo-pombe <theo.pombe@outlook.com>
2025-09-28 16:02:10 +0300

    feat(app): configure express app with security and utility middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,24 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false, // disable CORP for serving static files
+  })
+); // Set the security headers
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static("uploads"));
 
 export default app;
