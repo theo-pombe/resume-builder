72e8e4278040f4413f20bc4699c785c24060822f
theo-pombe <theo.pombe@outlook.com>
2025-10-15 23:08:29 +0300

    feat(api): add admin personal info operations (get all, get one, update, delete)

--- frontend/src/services/sections/personalInfo.ts
+++ frontend/src/services/sections/personalInfo.ts
@@ -1,10 +1,10 @@
 import type { FetchResponseType } from "app-shared";
-import api from "../../utilities/api";
 import type { PersonalInfoType } from "app-personalinfo";
+import api from "../../utilities/api";
 
 export const addPerson = async (resumeId: string, data: any) => {
   const res = await api.post<FetchResponseType<PersonalInfoType>>(
-    `resumes/${resumeId}/personal-information`,
+    `/resumes/${resumeId}/personal-information`,
     data
   );
 
@@ -12,9 +12,27 @@ export const addPerson = async (resumeId: string, data: any) => {
   return { success, message, person };
 };
 
+export const getPersons = async () => {
+  const res = await api.get<FetchResponseType<PersonalInfoType[]>>(
+    `/admin/personal-informations`
+  );
+
+  const { success, message, data } = res.data;
+  return { success, message, persons: data };
+};
+
 export const getPerson = async (resumeId: string) => {
   const res = await api.get<FetchResponseType<PersonalInfoType>>(
-    `resumes/${resumeId}/personal-information`
+    `/resumes/${resumeId}/personal-information`
+  );
+
+  const { success, message, data } = res.data;
+  return { success, message, person: data };
+};
+
+export const getPersonById = async (id: string) => {
+  const res = await api.get<FetchResponseType<PersonalInfoType>>(
+    `/admin/personal-informations/${id}`
   );
 
   const { success, message, data } = res.data;
@@ -23,10 +41,28 @@ export const getPerson = async (resumeId: string) => {
 
 export const editPerson = async (resumeId: string, data: any) => {
   const res = await api.patch<FetchResponseType<PersonalInfoType>>(
-    `resumes/${resumeId}/personal-information`,
+    `/resumes/${resumeId}/personal-information`,
     data
   );
 
   const { success, message, data: person } = res.data;
   return { success, message, person };
+};
+
+export const editPersonById = async (id: string) => {
+  const res = await api.patch<FetchResponseType<PersonalInfoType>>(
+    `/admin/personal-informations/${id}`
+  );
+
+  const { success, message, data } = res.data;
+  return { success, message, person: data };
+};
+
+export const deletePersonById = async (id: string) => {
+  const res = await api.delete<FetchResponseType<PersonalInfoType>>(
+    `/admin/personal-informations/${id}`
+  );
+
+  const { success, message, data } = res.data;
+  return { success, message, person: data };
 };
