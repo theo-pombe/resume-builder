c7f4fb9c1f95107cf36080fc58135dba91d36b04
theo-pombe <theo.pombe@outlook.com>
2025-10-17 10:03:13 +0300

    feat(messages): add faqs and faq details page for admin

---
+++ frontend/src/pages/admin/systems/FAQDetails.tsx
@@ -0,0 +1,9 @@
+const FAQDetails = () => {
+  return (
+    <div className="space-y-5 relative min-h-[84vh]">
+      <h2 className="text-xl font-semibold text-cyan-800">FAQ Details</h2>
+    </div>
+  );
+};
+
+export default FAQDetails;
---
+++ frontend/src/pages/admin/systems/FAQs.tsx
@@ -0,0 +1,9 @@
+const FAQs = () => {
+  return (
+    <div className="space-y-5 relative min-h-[84vh]">
+      <h2 className="text-xl font-semibold text-cyan-800">FAQs</h2>
+    </div>
+  );
+};
+
+export default FAQs;
