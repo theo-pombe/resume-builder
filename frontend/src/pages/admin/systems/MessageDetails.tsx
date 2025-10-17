af9d27cc7f5d01a42bc68a545e9ca15b6e36ae70
theo-pombe <theo.pombe@outlook.com>
2025-10-17 08:22:06 +0300

    feat(messages): add messages and message details page for admin

---
+++ frontend/src/pages/admin/systems/MessageDetails.tsx
@@ -0,0 +1,9 @@
+const MessageDetails = () => {
+  return (
+    <div className="space-y-5 relative min-h-[84vh]">
+      <h2 className="text-xl font-semibold text-cyan-800">Message Details</h2>
+    </div>
+  );
+};
+
+export default MessageDetails;
---
+++ frontend/src/pages/admin/systems/Messages.tsx
@@ -0,0 +1,9 @@
+const Messages = () => {
+  return (
+    <div className="space-y-5 relative min-h-[84vh]">
+      <h2 className="text-xl font-semibold text-cyan-800">Messages</h2>
+    </div>
+  );
+};
+
+export default Messages;
